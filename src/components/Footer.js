a1931147a4dcaf694afb94ebaa5eb9c79e4bcbf1
J-Krush <jkrush@pm.me>
2024-09-14 22:58:27 -0400

    refactor: update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,14 +6,14 @@ const Footer = () => {
   return (
     <footer
       className="w-full border-t-2 border-solid border-dark
-    font-medium text-lg dark:text-light dark:border-light sm:text-base
+    font-medium text-xs dark:text-light dark:border-light sm:text-xs
     "
     >
-      <Layout className="py-8 flex items-center justify-between lg:flex-col lg:py-6">
+      <Layout className="py-8 flex items-center justify-between flex-col py-6">
         <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
 
-        <div className="flex items-center lg:py-2">
-          Built with <span className="text-primary text-2xl px-1 dark:text-primaryDark">&#9825;	</span> by&nbsp;
+        <div className="flex items-center py-2">
+          Bootstrapped with <span className="text-primary text-xs px-1 dark:text-primaryDark">&#9825;	</span> by&nbsp;
           <Link
             href="https://devdreaming.com"
             target="_blank"
@@ -22,14 +22,6 @@ const Footer = () => {
             CodeBucks
           </Link>
         </div>
-
-        <Link
-          href="https://devdreaming.com/about"
-          target="_blank"
-          className="underline underline-offset-2"
-        >
-          Say Hello
-        </Link>
       </Layout>
     </footer>
   );
