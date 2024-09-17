92c7a188f58f1d44744deb4077d03a51428f3f9f
J-Krush <jkrush@pm.me>
2024-09-17 14:54:23 -0400

    refactor: change out hero image on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import lightBulb from "../../public/images/svgs/miscellaneous_icons_1.svg";
-import profilePic from "../../public/images/profile/openart-creative.png";
+import profilePic from "../../public/images/profile/creator-maker-in-nature-upscale-v2.png";
 import TransitionEffect from "@/components/TransitionEffect";
 import Background from "@/components/Background"
 
