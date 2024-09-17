770cf8dc661b600fcd566d2594ed291f5c0e3a13
J-Krush <jkrush@pm.me>
2024-09-17 17:47:33 -0400

    refactor: use lower res hero image for load time

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import lightBulb from "../../public/images/svgs/miscellaneous_icons_1.svg";
-import profilePic from "../../public/images/profile/creator-maker-in-nature-upscale-v2.png";
+import profilePic from "../../public/images/profile/creator-maker-in-nature-v2.png";
 import TransitionEffect from "@/components/TransitionEffect";
 import Background from "@/components/Background"
 
