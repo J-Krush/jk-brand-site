71c9d764faeb7e0b50d36c9325b95130f3aece22
J-Krush <jkrush@pm.me>
2024-09-18 11:59:42 -0400

    fix: event variable on contact form submission

--- src/pages/connect.js
+++ src/pages/connect.js
@@ -109,7 +109,7 @@ lg:p-8 xs:rounded-2xl  xs:rounded-br-3xl xs:p-4
             <button type="submit" className="rounded-lg
              bg-dark p-2 px-6 text-lg font-semibold text-light dark:bg-light dark:text-dark 
              sm:px-4 sm:text-base" 
-             onClick={() => handleSubmit()}
+             onClick={(e) => handleSubmit(e)}
              >
               Send Message
             </button>
