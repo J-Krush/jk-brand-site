ebe368197165d89672efa95c571d1fff086f991c
J-Krush <jkrush@pm.me>
2024-09-26 18:06:53 -0400

    refactor: add index.html redirect to /

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: async () => {
+    return [
+      {
+        source: '/index.html',
+        destination: '/',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
